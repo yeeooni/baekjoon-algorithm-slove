1126d1f473718881f4592f4856789d547685c5ad
EUIYEON KIM <12.12kimiyeon@gmail.com>
2024-10-28 11:02:42 +0900

    셸 스크립트 수정
    주간 통계 카운트 계산

--- shell-scripts/cal-months-algorithm-slove.sh
+++ shell-scripts/cal-months-algorithm-slove.sh
@@ -6,15 +6,24 @@ TODAY=$(date +%Y-%m-%d)
 # 사용자가 몇 개월 전부터 확인할지 입력받습니다.
 MONTHS_AGO=$1
 
-# 개월 수만큼 과거의 날짜를 계산합니다.
-PAST_DATE=$(date -d "$TODAY -${MONTHS_AGO} months" +%Y-%m-%d)
+# 6개월 전부터 현재까지의 Java 파일 최초 커밋 수 카운트
+START_DATE=$(date -d "${MONTHS_AGO} months ago" +%Y-%m-%d)
+TODAY=$(date +%Y-%m-%d)
 
-# 현재 디렉토리에서 .java 파일 중에서 과거부터 현재까지 생성된 파일 개수를 셉니다.
-JAVA_FILE_COUNT=$(find src -type f -name "*.java" -newermt "${PAST_DATE}" ! -newermt "${TODAY}" | wc -l)
+# 최초 커밋 개수를 저장할 변수 초기화
+FIRST_COMMIT_COUNT=$(git ls-files "*.java" | while read file; do
+    # 파일의 최초 커밋 날짜 조회
+    FIRST_COMMIT_DATE=$(git log --follow --diff-filter=A --format="%cs" -- "$file" | tail -n 1)
+
+    # 최초 커밋 날짜가 6개월 이내라면 해당 파일을 출력
+    if [[ "$FIRST_COMMIT_DATE" -g "$START_DATE" && "$FIRST_COMMIT_DATE" -le "$TODAY" ]]; then
+        echo "$file 최초 커밋 날짜: $FIRST_COMMIT_DATE"
+    fi
+done | wc -l)
 
 # 결과 출력
-echo "${MONTHS_AGO}개월 전부터 지금까지 생성된 자바 파일 수: ${JAVA_FILE_COUNT}개"
+echo "${MONTHS_AGO}개월 전부터 지금까지 생성된 자바 파일 수: ${FIRST_COMMIT_COUNT}개"
 
 # GitHub Actions 에서 사용할 환경 변수 파일에 기록
-echo "MONTH_AGO=$MONTHS_AGO" >> $GITHUB_ENV
-echo "JAVA_FILE_COUNT=$JAVA_FILE_COUNT" >> $GITHUB_ENV
+echo "MONTHS_AGO=$MONTHS_AGO" >> $GITHUB_ENV
+echo "FIRST_COMMIT_COUNT=$FIRST_COMMIT_COUNT" >> $GITHUB_ENV

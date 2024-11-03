2c34f19927b8efa89931555b64380986a7138ec8
EUIYEON KIM <12.12kimiyeon@gmail.com>
2024-11-03 19:00:11 +0900

    10809번
    알파벳 찾기 - String indexOf 활용 방법

---
+++ src/com/test/coding/baekjoon/math/$_11720.java
@@ -0,0 +1,37 @@
+package com.test.coding.baekjoon.strings;
+
+import java.io.*;
+
+/**
+ *
+ * $_11720 의 설명
+ * 패키지: com.test.coding.baekjoon.strings
+ * 알고리즘 분류:
+ * 문제: N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+ * 입력: 첫째 줄에 숫자의 개수 N (1 ≤ N ≤ 100)이 주어진다. 둘째 줄에 숫자 N개가 공백없이 주어진다.
+ * 출력: 입력으로 주어진 숫자 N개의 합을 출력한다.
+ * 주의사항:
+ * 일시: 2024. 10. 15.
+ * 정답율: 56%
+ * 수행시간: 104ms
+**/
+public class $_11720 {
+
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
+            int n = Integer.parseInt(br.readLine()), sum = 0;
+            char[] chars = br.readLine().toCharArray();
+
+            for(char c : chars){
+                sum += Character.getNumericValue(c);
+            }
+
+            bw.write(String.valueOf(sum));
+            bw.flush();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}

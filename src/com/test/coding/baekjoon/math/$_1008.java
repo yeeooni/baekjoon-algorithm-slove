dffeed8428da9951b3ce4d84a74d97a618111b7c
EUIYEON KIM <12.12kimiyeon@gmail.com>
2024-11-03 18:11:14 +0900

    1008번
    A/B - 사칙연산, 정수형과 더블형의 연산 범위에 대해서 생각해야 한다.

--- src/com/test/coding/baekjoon/math/$_1008.java
+++ src/com/test/coding/baekjoon/math/$_1008.java
@@ -1,21 +1,21 @@
-package com.test.coding.baekjoon.inoutput;
+package com.test.coding.baekjoon.math;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
 /**
  *
- * Solution_1008의 설명
- * 패키지: com.test.coding.baekjoon.inoutput
+ * $_1008 의 설명
+ * 패키지: com.test.coding.baekjoon.input_output_and_arithmetic_operations
+ * 알고리즘 분류: 수학, 구현, 사칙연산
  * 문제: 두 정수 A와 B를 입력받은 다음, A/B 출력
- * 입력: 첫째 줄 A, B (0 le A, B le 10)
+ * 입력: 첫째 줄 A, B (0 < A, B < 10)
  * 출력: 첫째 줄에 A/B 출력
- * 주의사항: 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다. (정수형은 사용하지 말라는 뜻)
- * 일시: 3/13/24
- * 정답율: 2023/3/10 시점 약 34%
- * 수행시간: 220ms -> 132ms 단축
+ * 주의사항: 실제 정답과 출력값의 절대오차 또는 상대오차가 10^-9 이하이면 정답이다.(정수형은 사용하지 말라는 뜻)
+ * 일시: 2024. 03. 13.
+ * 정답율: 34%
+ * 수행시간: 132ms
 **/
-
 public class $_1008 {
 
     public static void main(String[] args) {
@@ -26,8 +26,9 @@ public class $_1008 {
             double a = Double.parseDouble(st.nextToken()), b = Double.parseDouble(st.nextToken());
 
             bw.write(String.valueOf(a / b));
-        }catch (IOException ioe){
-            System.err.print(ioe.getMessage());
+            bw.flush();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
